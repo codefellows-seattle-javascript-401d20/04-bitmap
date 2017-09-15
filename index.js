4001a5bcb7efd6c3a60b1c05c97e7c29f0c13d46
Stuart Kershaw <stuartdkershaw@gmail.com>
2017-09-15 16:10:49 -0700

    Grabbing meta data

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const fs = require('fs')
+
+// //const read = (path, callback) => {
+fs.readFile('./__test__/asset/house.bmp', (err, data) => {
+  if(err) {
+    console.error(err)
+    return 
+  }
+  //console.log('header: ', data.slice(0, 2).toString())
+  //console.log('size in bytes: ', data.readUInt32LE(2, 4))
+  //console.log(data.readUInt32LE(10, 4))
+  //return data
+
+  //slice out the color array and mutate it, and the original buffer is changed
+  //
+  console.log('buffer: ', data)
+  console.log('type: ', data.slice(0, 2).toString())
+  console.log('size in bytes: ', data.readUInt32LE(2))
+  console.log('pixel array offset: ', data.readUInt32LE(10))
+  console.log('width in pixels: ', data.readUInt32LE(18))
+  console.log('height in pixels: ', data.readUInt32LE(22))
+  console.log('color planes: ', data.readUInt16LE(26))
+  console.log('bits per pixel: ', data.readUInt16LE(28))
+  console.log('compression method: ', data.readUInt16LE(30))
+  console.log('image size: ', data.readUInt32LE(34))
+  console.log('horizontal resolution: ', data.readUInt32LE(38))
+  console.log('vertical resolution: ', data.readUInt32LE(42))
+  console.log('number of colors in the color palette: ', data.readUInt32LE(46))
+  console.log('number of important colors used: ', data.readUInt32LE(50))
+
+  // fs.writeFile('./__test__/asset/new_house.bmp', data, (err) => {
+  //   if (err) throw err;
+  //   console.log('The file has been saved!');
+  // });
+})
+
+// fs.writeFile('./__test__/asset/new_house.bmp', './__test__/asset/house.bmp', (err) => {
+//   if (err) throw err;
+//   console.log('The file has been saved!');
+// });
+//}
+//  module.exports = {
+//    read: read, 
+//  };
+
