57cf04922aff4fb48bc1fb953204b954246387f6
Stuart Kershaw <stuartdkershaw@gmail.com>
2017-09-15 16:43:45 -0700

    Color invert

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ fs.readFile('./__test__/asset/house.bmp', (err, data) => {
   console.log('type: ', data.slice(0, 2).toString())
   console.log('size in bytes: ', data.readUInt32LE(2))
   console.log('pixel array offset: ', data.readUInt32LE(10))
+  console.log('size of header: ', data.readUInt32LE(14))
   console.log('width in pixels: ', data.readUInt32LE(18))
   console.log('height in pixels: ', data.readUInt32LE(22))
   console.log('color planes: ', data.readUInt16LE(26))
@@ -30,10 +31,17 @@ fs.readFile('./__test__/asset/house.bmp', (err, data) => {
   console.log('number of colors in the color palette: ', data.readUInt32LE(46))
   console.log('number of important colors used: ', data.readUInt32LE(50))
 
-  // fs.writeFile('./__test__/asset/new_house.bmp', data, (err) => {
-  //   if (err) throw err;
-  //   console.log('The file has been saved!');
-  // });
+  let color_table = data.slice(41, 1065)
+  for (let i = 0; i < color_table.length; i++) {
+    //color_table[i] = -255;
+    color_table[i] = 255 - color_table[i];
+  }
+  console.log(color_table)
+
+  fs.writeFile('./__test__/asset/new_house.bmp', data, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
 })
 
 // fs.writeFile('./__test__/asset/new_house.bmp', './__test__/asset/house.bmp', (err) => {
