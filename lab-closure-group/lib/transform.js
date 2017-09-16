68c95880ff3ebee9e564c8e10419a6b136292d42
Anthony Robinson <robinsonamk@gmail.com>
2017-09-15 17:59:48 -0700

    got invert to work

--- lab-closure-group/lib/transform.js
+++ lab-closure-group/lib/transform.js
@@ -1,20 +1,21 @@
 'use strict';
 
 // Require each transform modules here...
-const grayscale = require(`./transformations/grayscale.js`);
+// const grayscale = require(`./transformations/grayscale.js`);
+const invert = require(`./transformations/invert.js`);
 
 module.exports = (object, option) => {
   // Parse the transform flags/options and call their respective modules for transformation of the object.
   // Maybe a switch statement? IDK..
 
   switch(option) {
-  case 'grayscale':
-    grayscale(object);
+  case 'invert':
+    invert(object);
     break;
   default:
     break;
   }
 
-  object.colorTable.fill(150);
+  // object.colorTable.fill(150);
 
 };
