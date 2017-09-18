6cbff6bd4dc6467377619561953f63f8760862b0
Ron Barrantes <ronbarrantes@gmail.com>
2017-09-18 07:45:23 -0700

    added cyan magenta and yellow transformation

--- lab-closure-group/lib/transformations/flipUp.js
+++ lab-closure-group/lib/transformations/flipUp.js
@@ -1,11 +1,11 @@
 'use strict';
 
 module.exports = (object) => {
-  let length = object.pixelArray.length/2;
-  for(let i = 0; i < length; i++) {
-    let change = object.pixelArray.readUInt8(object.pixelArray.length-1-i);
+  let length = object.pixelArray.length / 2;
+  for (let i = 0; i < length; i++) {
+    let change = object.pixelArray.readUInt8(object.pixelArray.length - 1 - i);
     let temp = object.pixelArray.readUInt8(i);
     object.pixelArray.writeUInt8(change, i);
-    object.pixelArray.writeUInt8(temp, object.pixelArray.length-1-i);
+    object.pixelArray.writeUInt8(temp, object.pixelArray.length - 1 - i);
   }
 };
