0fca3e55b13f1f2c332898576983bdaeaf02fb92
Stuart Kershaw <stuartdkershaw@gmail.com>
2017-09-18 10:18:32 -0700

    Refactor from class demo

--- __test__/transforms.test.js
+++ __test__/transforms.test.js
@@ -10,9 +10,9 @@ describe('transform', () => {
   test('whiteout', (done) => {
     fs.readFile(`${__dirname}/asset/house.bmp`, function(err, data) {
       if (err) throw err;
-      let old_color_table = data.buffer.slice(41, 1065);
-      let new_color_table = bitmap_whiteout.slice(41, 1065);
-      expect(old_color_table).not.toEqual(new_color_table);
+      let old_colorTable = data.buffer.slice(41, 1065);
+      let new_colorTable = bitmap_whiteout.slice(41, 1065);
+      expect(old_colorTable).not.toEqual(new_colorTable);
       done();
     });
   });
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if (process.argv[2] && process.argv[3] && process.argv[4]) {
   let infilename = process.argv[2];
   let outfilename = process.argv[3];
   let transform = process.argv[4];
-  infile(infilename, transforms[transform], outfilename)
+  infile(infilename, transforms[transform], outfilename);
 } else {
-  throw new Error('infile, outfile, and transform required');
+  console.log('USAGE ERROR: infile, outfile, and transform arguments required');
 }
