d559d0266736fe426d08ba22f7e35c9255bb001c
Stuart Kershaw <stuartdkershaw@gmail.com>
2017-09-18 08:42:19 -0700

    Cleaned up for submission

--- __test__/transforms.test.js
+++ __test__/transforms.test.js
@@ -9,9 +9,9 @@ const bitmap_whiteout = fs.readFileSync(`${__dirname}/asset/house_new.bmp`);
 describe('transform', () => {
   test('whiteout', (done) => {
     fs.readFile(`${__dirname}/asset/house.bmp`, function(err, data) {
-      if (err) throw err
-      let old_color_table = data.buffer.slice(41, 1065)
-      let new_color_table = bitmap_whiteout.slice(41, 1065)
+      if (err) throw err;
+      let old_color_table = data.buffer.slice(41, 1065);
+      let new_color_table = bitmap_whiteout.slice(41, 1065);
       expect(old_color_table).not.toEqual(new_color_table);
       done();
     });
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const infile = require('./lib/infile.js')
-const transforms = require('./lib/transforms.js')
+const infile = require('./lib/infile.js');
+const transforms = require('./lib/transforms.js');
 
 if (process.argv[2] && process.argv[3] && process.argv[4]) {
-  let infilename = process.argv[2]
-  let outfilename = process.argv[3]
-  let transform = process.argv[4]
+  let infilename = process.argv[2];
+  let outfilename = process.argv[3];
+  let transform = process.argv[4];
   infile(infilename, transforms[transform], outfilename)
 } else {
-  throw new Error('infile, outfile, and transform required')
+  throw new Error('infile, outfile, and transform required');
 }
